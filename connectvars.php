1d5567a4b3fea6af977973fd546c56e4a38b804f
Anjana2002 <anjanaa628@gmail.com>
2024-07-21 11:56:45 +0530

    Remove connectvars.php from version control

--- connectvars.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // Define database connection constants
-  define('DB_HOST', 'localhost:3306');
-  define('DB_USER', 'anjana');
-  define('DB_PASSWORD', 'anilamma@123');
-  define('DB_NAME', 'nascsms');
-
-?>
